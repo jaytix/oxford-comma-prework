bbb6f09d24db53ebc0fbbff239b9a7ac6a996656
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-16 20:01:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif array.count == 3
     "#{array[0,2].join(", ")}, and #{array.last}"
   else
-    "#{array[0,3].join(", ")}, and #{array.last}"
+    "#{array[0,4].join(", ")}, and #{array.last}"
   end
 end
